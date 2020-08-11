46b2d8ebfcb154e62ca7d9f4dc6dbcf78b5f2280
TeaWhyDee <daniel.555365@gmail.com>
2020-08-11 08:26:05 +0300

    little Task1 upd

--- Day1/HW1_Task1.cpp
+++ Day1/HW1_Task1.cpp
@@ -30,7 +30,7 @@ bool triangleExists(int x1, int y1, int x2, int y2, int x3, int y3){
 }
 
 
-int main(int argc, char const *argv[])
+int main()
 {   
     cout << "Enter 6 numbers (x1 y1 x2 y2 x3 y3): ";
     int Pts[6];  // Pts = Points. Initialize and read all Points of the triangle (x1, y1, x2, y2, x3, y3)
